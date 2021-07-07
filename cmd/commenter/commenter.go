89c691a931c555965b3f2c85857f7aa08980e311
Owen Rumney <owen.rumney@aquasec.com>
2021-07-07 15:48:50 +0100

    Add more info in loggin
    - update stdout logging
    - fix issues with entrypoint

--- cmd/commenter/commenter.go
+++ cmd/commenter/commenter.go
@@ -51,11 +51,15 @@ func main() {
 			// don't error if its simply that the comments aren't valid for the PR
 			switch err.(type) {
 			case commenter.CommentAlreadyWrittenError:
+				fmt.Println("Comment already written so not writing")
 			case commenter.CommentNotValidError:
+				fmt.Printf("Comment not written [%s], not part of the current PR\n", result.Description)
 				continue
 			default:
 				errMessages = append(errMessages, err.Error())
 			}
+		} else {
+			fmt.Printf("Writing comment to %s:%d:%d", result.Range.Filename, result.Range.StartLine, result.Range.EndLine)
 		}
 	}
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,7 @@ if [ -n "${GITHUB_WORKSPACE}" ]; then
   cd "${GITHUB_WORKSPACE}" || exit
 fi
 
-if ! tfsec --format=json "${INPUT_WORKING_DIRECTORY}" > results.json
-then
-  echo "tfsec errors occurred, running commenter..."
+if ! tfsec --format=json "${INPUT_WORKING_DIRECTORY}" >results.json; then
+  echo "tfsec violations were identified, running commenter..."
   commenter
 fi
