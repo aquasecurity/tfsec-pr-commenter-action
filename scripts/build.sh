d57ef181da911a543b8e5c7da7590acf0148bb57
Szymon Szypulski <szymon.szypulski@gmail.com>
2022-11-22 07:50:54 +0100

    Add support for the arm64 architecture
    It is for people trying to run an arm64-only infrastructure (for
    example, Gravitron). From my testing, we do not need any other changes.

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,3 +2,4 @@
 
 export GO111MODULE=auto
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o bin/commenter-linux-amd64 ./cmd/commenter
+GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -o bin/commenter-linux-arm64 ./cmd/commenter
